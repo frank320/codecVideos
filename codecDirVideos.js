dd0b311c5808db46a3324f203f04c716ae78dcc5
frank@tuxing <zhangshipeng@tuxing2010.com>
2017-07-31 11:20:24 +0800

    xxx

--- codecDirVideos.js
+++ codecDirVideos.js
@@ -57,14 +57,31 @@
           //ffmpeg.setFfprobePath(path.join(__dirname, './ffmpeg/ffprobe.exe'))
 
           new ffmpeg({source: VideoPath})
-            .withVideoBitrate('512k')
-            .size('1280x720')
-            .withVideoCodec('libx264')
-            .withAudioBitrate('96k')
-            .audioCodec('aac')
-            .audioFrequency(48000)
-            .audioChannels(2)
-            .saveToFile(path.join(bundleDir, `${videoName}.mp4`))
+            //.withVideoBitrate('512k')
+            ////.size('1280x720')
+            //.withVideoCodec('libx264')
+            //.withAudioBitrate('96k')
+            //.audioCodec('aac')
+            //.audioFrequency(48000)
+            //.audioChannels(2)
+            //.outputOptions('-vf crop=1280:720:0:0')
+            ////.outputOptions('-vf cropdetect=limit=0')
+            .outputOptions([
+              '-b 6000000',
+              '-bf 2',
+              '-r 25',
+              '-maxrate 6000000',
+              '-minrate 6000000',
+              '-bufsize 6400000',
+              '-muxrate 8000000',
+              '-max_delay 800000',
+              '-vcodec libx264',
+              '-x264opts keyint=50:qcomp=1.0:nal-hrd=cbr:threads=3:sliced_threads:qpmin=15:aud:force-cfr:b-pyramid=none',
+              '-acodec mp2',
+              '-ar 48000',
+              '-b:a 192k'
+            ])
+            .saveToFile(path.join(bundleDir, `${videoName}.ts`))
             .on('error', function (err) {
               console.log(`${bundleName}  ${videoName} 转码失败 (${index + 1}/${totalCount})====>${err}`)
               resolve('fail')
@@ -83,29 +100,5 @@
   }
 
   codecVideos()
-  // 遍历转码
-  //contentFiles.forEach(function (video) {
-  //  const VideoPath = path.join(originalDir, video)
-  //  if (isDir(VideoPath)) {
-  //    return console.log('目录下不能出现文件夹')
-  //  }
-  //  const videoName = getNoneExtFileName(video)
-  //
-  //  //开始转码
-  //  //ffmpeg.setFfmpegPath(path.join(__dirname, './ffmpeg/ffmpeg.exe'))
-  //  //ffmpeg.setFfprobePath(path.join(__dirname, './ffmpeg/ffprobe.exe'))
-  //
-  //  new ffmpeg({source: VideoPath})
-  //    .withVideoBitrate('512k')
-  //    .size('1280x?')
-  //    .withVideoCodec('libx264')
-  //    .withAudioBitrate('96k')
-  //    .saveToFile(path.join(bundleDir, `${videoName}.mp4`))
-  //    .on('error', function (err) {
-  //      console.log(`${bundleName}  ${videoName} 转码失败====>${err}`)
-  //    })
-  //    .on('end', function () {
-  //      console.log(`${bundleName}  ${videoName} 转码成功`)
-  //    })
-  //})
+
 })()
